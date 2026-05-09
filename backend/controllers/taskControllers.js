ece228381581678cd8bea01869badc8b3def2bce
Kristi <sekujkristi@gmail.com>
2026-05-09 11:30:31 +0200

    shtova lidhjen me MongoDB

--- backend/controllers/taskControllers.js
+++ backend/controllers/taskControllers.js
@@ -1,21 +1,43 @@
-const getTasks = (req, res) => {
-    res.status(200).json({ message: 'Get All Tasks' })
-}
+const asyncHandler = require ('express-async-handler');
+const Task = require ('../models/taskModel');
 
-const setTask = (req, res) => {
-    if (!req.body || !req.body.text) {
+const getTasks = asyncHandler(async (req,res) => {
+    const tasks = await Task.find()
+    res.status(200).json(tasks)
+})
+
+const setTask = asyncHandler(async (req,res)) => {
+    if( !req.body || !req.body.text) {
         res.status(400)
-        throw new Error('Please enter a text');
+        throw new Error('Please enter a task')
+
+}
+    res.status(200).json ({message: 'Create Task'});
+
+}
+
+const task = await Task.create ({ text: req.body.text})
+res.status(200).json(task);
+
+
+const updateTask = asyncHandler(async (req,res)) => {
+    const task = await Task.findById(req.params.id);
+if (!task) {
+    res.status(400);
+    throw new Error('Task not found');
     }
-    res.status(200).json({ message: 'Create task' })
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {new:true});
+    res.status(200).json(updatedTask);
 }
 
-const updateTask = (req, res) => {
-    res.status(200).json({ message: `Task ${req.params.id} updated.` })
-}
+const deleteTask = asyncHandler(async(req,res) => {
+    const task = await Task.findById(req.params.id);
 
-const deleteTask = (req, res) => {
-    res.status(200).json({ message: `Task ${req.params.id} deleted.` })
-}
+    if(!task) {
+        res.status(400);
+        throw new Error('Task not found');
+    }
+    await Task.findByIdandDelete(req.params.id);
+    res.status(200).json({id: req.params.id});
 
-module.exports = { getTasks, setTask, updateTask, deleteTask }
+})
